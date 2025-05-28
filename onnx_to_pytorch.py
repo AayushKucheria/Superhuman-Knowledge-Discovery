b9998dfb7d3628d8d1c0c8c4525599763df2a8fb
AayushKucheria <aayush.kucheria@gmail.com>
2025-05-28 17:46:06 +0300

    restructured repo

--- onnx_to_pytorch.py
+++
@@ -1,6 +0,0 @@
-from onnx2torch import convert
-import onnx
-
-onnx_model_path = "path/to/your/model.onnx"
-onnx_model = onnx.load(onnx_model_path)
-torch_model = convert(onnx_model)
--- scripts/onnx_to_pytorch.py
+++ scripts/onnx_to_pytorch.py
@@ -3,13 +3,8 @@ import torch.onnx
 import onnx
 from onnx2torch import convert
 
-# Load the ONNX Model
 onnx_model = onnx.load("t3-512x15x16h-distill-swa-2767500.onnx")
-
-# Convert to pytorch
 pytorch_model = convert(onnx_model)
-
-print(type(pytorch_model))
 
 torch.save(pytorch_model, "t3-512x15x16h-distill-swa-2767500.pth")
 
